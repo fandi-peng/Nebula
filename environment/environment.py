56cdac68bddb9f96d43bbafd126fd0af171dad3d
Fandi Peng <fandi@gatech.edu>
2015-05-13 11:23:35 +0800

    update

--- environment/environment.py
+++ environment/environment.py
@@ -2,6 +2,10 @@
 # sudo apt-get install python-dev
 # sudo python get-pip.py
 # sudo pip install fabric
+# sudo apt-get install zsh
+# chsh -s $(which zsh)
+# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8000
+
 
 import subprocess
 
