5e8cc6f5b8307374906f7f975f0b4a41dfb0d89f
Fandi Peng <fandi@gatech.edu>
2015-05-15 21:57:09 +0800

    update

---
+++ environment/env_setup.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+sudo true
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"' >> ~/.bashrc
+
+source ~/.bashrc
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
+
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+pyenv install 2.7.9
+
+pyenv rehash     # do this any time you install a new Python binary
+
+pyenv global 2.7.9
+
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+
+pyenv virtualenvwrapper_lazy -y
+
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8000
+
+
+source ~/.bashrc
