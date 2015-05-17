d6ca9d019d34b019bb14160afb46f46877e6330a
Fandi Peng <fandi@gatech.edu>
2015-05-17 20:48:02 +0800

    update

--- environment/env_setup.sh
+++ environment/env_setup.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 sudo true
 
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8000
+
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
@@ -19,11 +21,12 @@ pyenv rehash     # do this any time you install a new Python binary
 
 pyenv global 2.7.9
 
+sudo curl https://bootstrap.pypa.io/get-pip.py | python
+
 git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
 
-pyenv virtualenvwrapper_lazy -y
+pyenv virtualenvwrapper_lazy
 
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8000
 
 
 source ~/.bashrc
