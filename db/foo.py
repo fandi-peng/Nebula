6949e8405c188e896d077ebd431c7f878abf248d
Fandi Peng <fandi@gatech.edu>
2015-05-19 20:28:00 +0800

    update

---
+++ db/foo.py
@@ -0,0 +1,9 @@
+import pymongo
+
+# Connection to Mongo DB
+try:
+    conn=pymongo.MongoClient()
+    print "Connected successfully!!!"
+except pymongo.errors.ConnectionFailure, e:
+   print "Could not connect to MongoDB: %s" % e 
+conn
